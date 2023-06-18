bac8c94575b55db7199647cb38839f3b9012d6eb
mRIDULhedayet <119140283+mRIDULhedayet@users.noreply.github.com>
2023-06-19 03:46:13 +0600

    Create timeConversion.php

---
+++ timeConversion.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion($s) {
+$time12 = new DateTime($s);
+$time24 = $time12->format('H:i:s');
+
+return $time24;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$s = rtrim(fgets(STDIN), "\r\n");
+
+$result = timeConversion($s);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
+?>
