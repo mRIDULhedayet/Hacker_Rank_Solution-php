753a488a719517aa464fdfc185328202ac5d2dec
mRIDULhedayet <119140283+mRIDULhedayet@users.noreply.github.com>
2023-06-19 03:18:54 +0600

    Update solveMeFirst.php

--- solveMeFirst.php
+++ solveMeFirst.php
@@ -1,1 +1,17 @@
+<?php
+
+function solveMeFirst($a,$b){
+  
+  $sum=$a+$b;
+  return $sum; 
+  
+}
+
+$handle = fopen ("php://stdin","r");
+$_a = fgets($handle);
+$_b = fgets($handle);
+$sum = solveMeFirst((int)$_a,(int)$_b);
+print ($sum);
+fclose($handle);
+?>
 
